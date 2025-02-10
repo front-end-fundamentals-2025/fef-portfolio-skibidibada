dcab8c9d8d62c352df716379375c870570e32c8a
brr <skibidibada@users.noreply.github.com>
2025-02-11 00:04:41 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 document.getElementById("planeinvis").style.display = "none";
 document.getElementById("fireinvis").style.display = "none";
 
+document.getElementById("dontClk").addEventListener("click", dontClick);
+
 window.dontClick = function () {
   document.getElementById("planeinvis").style.display = "block";
   document.getElementById("fireinvis").style.display = "block";
 };
 
-document.getElementById("dontClk").addEventListener("click", dontClick);
 // aaa
