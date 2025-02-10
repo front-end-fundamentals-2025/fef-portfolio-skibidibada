e5ae5e46fdf05c25a48c63952595cae068f8858f
brr <skibidibada@users.noreply.github.com>
2025-02-11 00:17:10 +0100

    fixed AGAIN

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 document.getElementById("planeinvis").style.display = "none";
 document.getElementById("fireinvis").style.display = "none";
 
-document.getElementById("dontClk").addEventListener("click", dontClick);
-
-window.dontClick = function () {
+document.getElementById("dontClk").addEventListener("click", function (event) {
   document.getElementById("planeinvis").style.display = "block";
   document.getElementById("fireinvis").style.display = "block";
-};
+});
+
+// window.dontClick = function () {
+
+// };
 
 // aaa
