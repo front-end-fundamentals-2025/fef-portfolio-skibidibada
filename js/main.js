fa55544f78b6b09c180dc6afec9d153e2c82dbcf
brr <skibidibada@users.noreply.github.com>
2025-02-10 23:46:44 +0100

    plane button

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("planeinvis").style.display = "none";
+document.getElementById("fireinvis").style.display = "none";
+
+window.dontClick = function () {
+  document.getElementById("planeinvis").style.display = "block";
+  document.getElementById("fireinvis").style.display = "block";
+};
