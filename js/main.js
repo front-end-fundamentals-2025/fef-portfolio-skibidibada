3b54bc137868b70aa480b63dcf379ec18261692f
brr <skibidibada@users.noreply.github.com>
2025-02-28 09:08:40 +0100

    Revert "fixed AGAIN"
    This reverts commit e5ae5e46fdf05c25a48c63952595cae068f8858f.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 document.getElementById("planeinvis").style.display = "none";
 document.getElementById("fireinvis").style.display = "none";
 
-document.getElementById("dontClk").addEventListener("click", function (event) {
+document.getElementById("dontClk").addEventListener("click", dontClick);
+
+window.dontClick = function () {
   document.getElementById("planeinvis").style.display = "block";
   document.getElementById("fireinvis").style.display = "block";
-});
-
-// window.dontClick = function () {
-
-// };
+};
 
 // aaa
